81aeea5d27369bfe30d405c1076f261eb0841eb9
Riari <3583774+Riari@users.noreply.github.com>
2020-12-02 10:28:23 +0000

    Day 2

---
+++ src/bin/2.rs
@@ -0,0 +1,66 @@
+use std::ops::RangeInclusive;
+use util;
+
+struct PasswordModel {
+    accepted_range: RangeInclusive<i32>,
+    character: char,
+    password: String,
+}
+
+fn values() -> Vec<PasswordModel> {
+    let rows: Vec<String> = util::file_to_vec("input/2");
+    let mut values = Vec::<PasswordModel>::new();
+
+    for row in rows.iter() {
+        let parts: Vec<&str> = row.split_whitespace().collect();
+        let range: Vec<&str> = parts[0].split('-').collect();
+        let character = parts[1].chars().nth(0).unwrap();
+        let password = parts[2];
+
+        values.push(PasswordModel {
+            accepted_range: (range[0].parse().unwrap()..=range[1].parse().unwrap()),
+            character,
+            password: password.to_owned()
+        });
+    }
+
+    return values;
+}
+
+fn part1() {
+    let values = values();
+
+    let mut result: usize = 0;
+    for value in values.iter() {
+        let occurrences = value.password.matches(value.character).count() as i32;
+        
+        if value.accepted_range.contains(&occurrences) {
+            result = result + 1;
+        }
+    }
+
+    println!("Part 1: {}", result);
+}
+
+fn part2() {
+    let values = values();
+
+    let mut result: usize = 0;
+    for value in values.iter() {
+        let mut chars = String::from("");
+        chars.push(value.password.chars().nth((value.accepted_range.start() - 1) as usize).unwrap());
+        chars.push(value.password.chars().nth((value.accepted_range.end() - 1) as usize).unwrap());
+        let occurrences = chars.matches(value.character).count();
+
+        if occurrences == 1 {
+            result = result + 1;
+        }
+    }
+
+    println!("Part 2: {}", result);
+}
+
+fn main() {
+    part1();
+    part2();
+}
--- src/util.rs
+++ src/util.rs
@@ -2,12 +2,17 @@ use std::{
     fs::File,
     io::{prelude::*, BufReader},
     path::Path,
+    str::FromStr,
 };
 
-pub fn file_to_vec(filename: impl AsRef<Path>) -> Vec<i32> {
+pub fn file_to_vec<T>(filename: impl AsRef<Path>) -> Vec<T>
+where
+    T: FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
     let file = File::open(filename).expect("no such file");
     let buf = BufReader::new(file);
     buf.lines()
-        .map(|l| l.expect("Could not parse line").parse().unwrap())
+        .map(|l| l.expect("Could not parse line").parse::<T>().unwrap())
         .collect()
 }
