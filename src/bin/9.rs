83409a695775dae6fff3c9f322ce697c854249aa
Riari <3583774+Riari@users.noreply.github.com>
2020-12-09 10:18:55 +0000

    Tweak day 9

--- src/bin/9.rs
+++ src/bin/9.rs
@@ -2,7 +2,25 @@ use std::cmp::Ordering;
 use std::ops::Add;
 use util;
 
-fn find_pair_summing_to<T>(slice: &[T], sum: T) -> Option<(usize, usize)>
+struct Row {
+    index: usize,
+    value: usize,
+}
+
+fn find_first_invalid_row(input: &Vec<usize>) -> Option<Row> {
+    let preamble = 25;
+    for (i, n) in input.iter().skip(preamble).enumerate() {
+        let slice: &[usize] = &input[i..preamble + i];
+
+        if !contains_pair_summing_to(slice, *n) {
+            return Some(Row { index: preamble + i, value: *n });
+        }
+    }
+
+    None
+}
+
+fn contains_pair_summing_to<T>(slice: &[T], sum: T) -> bool
 where
     T: Add<Output = T> + Ord + Copy,
 {
@@ -14,43 +32,29 @@ where
  
     while i < j {
         match (s[i] + s[j]).cmp(&sum) {
-            Ordering::Equal => return Some((i, j)),
+            Ordering::Equal => return true,
             Ordering::Less => i += 1,
             Ordering::Greater => j -= 1,
         }
     }
  
-    None
+    false
 }
 
-fn find_first_invalid_number(input: &Vec<usize>) -> Option<(usize, usize)> {
-    let preamble = 25;
-    for (i, n) in input.iter().skip(preamble).enumerate() {
-        let slice: &[usize] = &input[i..preamble + i];
-        let result = find_pair_summing_to(slice, *n);
-
-        if result.is_none() {
-            return Some((preamble + i, *n));
-        }
-    }
-
-    None
-}
-
-fn find_set_summing_to(input: &Vec<usize>, i: usize, value: usize) -> Option<Vec<usize>> {
-    let slice = &input.to_owned()[0..i];
+fn find_vec_summing_to(input: &Vec<usize>, row: &Row) -> Option<Vec<usize>> {
+    let slice = &input.to_owned()[0..row.index];
 
     let mut sum = slice[0];
     let mut start = 0;
     let mut end = 1;
 
     loop {
-        while sum < value && end < i {
+        while sum < row.value && end < row.index {
             sum = sum + slice[end];
             end += 1;
         }
 
-        if sum == value {
+        if sum == row.value {
             return Some(slice[start..end].to_vec());
         }
 
@@ -61,11 +65,10 @@ fn find_set_summing_to(input: &Vec<usize>, i: usize, value: usize) -> Option<Vec
 
 fn main() {
     let input: Vec<usize> = util::file_to_vec("input/9");
-    let first_invalid = find_first_invalid_number(&input).unwrap();
-    let mut set_summing_to = find_set_summing_to(&input, first_invalid.0, first_invalid.1).unwrap();
-    set_summing_to.sort();
-    let final_sum = set_summing_to[0] + set_summing_to[set_summing_to.len() - 1];
+    let first_invalid_row = find_first_invalid_row(&input).unwrap();
+    let vec_summing_to = find_vec_summing_to(&input, &first_invalid_row).unwrap();
+    let final_sum = vec_summing_to.iter().min().unwrap() + vec_summing_to.iter().max().unwrap();
 
-    println!("Part 1: {}", first_invalid.1);
+    println!("Part 1: {}", first_invalid_row.value);
     println!("Part 2: {}", final_sum);
 }
