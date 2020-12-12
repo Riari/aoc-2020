4614073009c76827a8b1b6f592e8399c6d469e90
Riari <3583774+Riari@users.noreply.github.com>
2020-12-12 18:07:15 +0000

    Finish day 12 part 2 (Ship struct was a bad idea...)

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -2,6 +2,18 @@ use util;
 
 struct Point(isize, isize);
 
+enum Cardinal {
+    North,
+    East,
+    South,
+    West,
+}
+
+struct Instruction {
+    action: char,
+    value: isize,
+}
+
 struct Ship {
     angle: isize,
     position: Point,
@@ -10,25 +22,18 @@ struct Ship {
 }
 
 impl Ship {
-    fn new() -> Ship {
+    fn new(waypoint: Option<Point>) -> Ship {
+        let use_waypoint = waypoint.is_some();
         Ship {
             angle: 90, // Starts by facing east
             position: Point(0, 0),
-            waypoint: Point(1, 1),
-            use_waypoint: false,
+            waypoint: waypoint.unwrap_or(Point(0, 0)),
+            use_waypoint: use_waypoint,
         }
     }
 
-    fn set_waypoint(&mut self, value: Point) {
-        self.waypoint = value;
-    }
-
-    fn set_use_waypoint(&mut self, use_waypoint: bool) {
-        self.use_waypoint = use_waypoint;
-    }
-
     fn get_cardinal(&self) -> Cardinal {
-        match self.angle % 360 {
+        match ((self.angle % 360) + 360) % 360 {
             0 => Cardinal::North,
             90 => Cardinal::East,
             180 => Cardinal::South,
@@ -45,43 +50,54 @@ impl Ship {
             'W' => self.translate(Cardinal::West, inst.value),
             'L' => self.rotate(inst.action, inst.value),
             'R' => self.rotate(inst.action, inst.value),
-            'F' => self.translate(self.get_cardinal(), inst.value),
+            'F' => {
+                if self.use_waypoint {
+                    self.translate_to_waypoint(inst.value);
+                } else {
+                    self.translate(self.get_cardinal(), inst.value)
+                }
+            },
             _ => panic!("Invalid instruction"),
         }
     }
 
     fn rotate(&mut self, direction: char, value: isize) {
-        let mut v = value;
-        if direction == 'L' && value != 180 {
-            v += 180;
+        let v = if direction == 'L' { -value } else { value };
+        if self.use_waypoint {
+            for _ in 0..((((v / 90) % 4) + 4) % 4) {
+                self.waypoint = Point(self.waypoint.1, -self.waypoint.0);
+            }
+        } else {
+            self.angle += v;
         }
-        self.angle += v % 360;
     }
 
     fn translate(&mut self, cardinal: Cardinal, value: isize) {
-        match cardinal {
-            Cardinal::North => self.position.1 += value,
-            Cardinal::South => self.position.1 -= value,
-            Cardinal::East => self.position.0 += value,
-            Cardinal::West => self.position.0 -= value,
+        if self.use_waypoint {
+            match cardinal {
+                Cardinal::North => self.waypoint.1 += value,
+                Cardinal::South => self.waypoint.1 -= value,
+                Cardinal::East => self.waypoint.0 += value,
+                Cardinal::West => self.waypoint.0 -= value,
+            }
+        } else {
+            match cardinal {
+                Cardinal::North => self.position.1 += value,
+                Cardinal::South => self.position.1 -= value,
+                Cardinal::East => self.position.0 += value,
+                Cardinal::West => self.position.0 -= value,
+            }
         }
+    }
+
+    fn translate_to_waypoint(&mut self, value: isize) {
+        self.position.0 += value * self.waypoint.0;
+        self.position.1 += value * self.waypoint.1;
     }
 }
 
-enum Cardinal {
-    North,
-    East,
-    South,
-    West,
-}
-
-struct Instruction {
-    action: char,
-    value: isize,
-}
-
 fn part1(instructions: &Vec<Instruction>) -> isize {
-    let mut ship = Ship::new();
+    let mut ship = Ship::new(None);
 
     for inst in instructions {
         ship.execute(&inst);
@@ -91,9 +107,7 @@ fn part1(instructions: &Vec<Instruction>) -> isize {
 }
 
 fn part2(instructions: &Vec<Instruction>) -> isize {
-    let mut ship = Ship::new();
-    ship.set_use_waypoint(true);
-    ship.set_waypoint(Point(10, 1));
+    let mut ship = Ship::new(Some(Point(10, 1)));
 
     for inst in instructions {
         ship.execute(&inst);
@@ -103,8 +117,7 @@ fn part2(instructions: &Vec<Instruction>) -> isize {
 }
 
 fn main() {
-    let input: Vec<String> = util::file_to_vec("input/12");
-    let instructions: Vec<Instruction> = input.iter()
+    let instructions: Vec<Instruction> = util::file_to_string("input/12").lines()
         .map(|l| Instruction { action: l.chars().nth(0).unwrap(), value: l.get(1..).unwrap().parse().unwrap() })
         .collect();
 
